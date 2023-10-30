2f1ca7e4b0777bc12d32890f7e7d2b26bff640e3
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-10-30 14:55:51 +0200

    add pagination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,8 +1,15 @@
+"use client";
+
 import React from "react";
 import styles from "./pagination.module.css";
 
 const Pagination = () => {
-  return <div className={styles.container}>Pagination</div>;
+  return (
+    <div className={styles.container}>
+      <button className={styles.button}>Previous</button>
+      <button className={styles.button}>Next</button>
+    </div>
+  );
 };
 
 export default Pagination;
