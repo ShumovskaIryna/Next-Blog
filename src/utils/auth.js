23dd44fee6bdabc3fbc2920ee24631401a5cd496
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-11-04 13:50:39 +0200

    use Prisma studio

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,11 @@
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./connect";
+import { getServerSession } from "next-auth";
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -13,3 +17,5 @@ export const authOptions = {
     }),
   ],
 };
+
+export const getAuthSession = () => getServerSession(authOptions);
