887e19a27e9477fe86ec7fae1221d4d6962bada1
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-11-06 11:39:06 +0200

    add page for post

---
+++ src/app/posts/[slug]/page.jsx
@@ -0,0 +1,36 @@
+import Menu from "@/components/Menu/Menu";
+import styles from "./singlePage.module.css";
+import Image from "next/image";
+import Comments from "@/components/comments/Comments";
+
+const SinglePage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.textContainer}>
+          <div className={styles.user}>
+            <div className={styles.userImageContainer}>
+              <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
+            </div>
+            <div className={styles.userTextContainer}>
+              <span className={styles.date}>01.01.2024</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="" alt="" fill className={styles.image} />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.post}>
+          <div className={styles.comment}>
+            <Comments />
+          </div>
+        </div>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default SinglePage;
