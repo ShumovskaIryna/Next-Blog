d6a7500ceab976c8dc4ad78d39b3217ce3f1dbfe
Iryna Shumovska <shumovska_ia@groupbwt.com>
2024-01-02 23:00:20 +0200

    create comment

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -1,5 +1,6 @@
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
+import { getAuthSession } from "@/utils/auth";
 
 // GET ALL COMMENTS OF A POST
 export const GET = async (req) => {
@@ -13,6 +14,31 @@ export const GET = async (req) => {
       include: {user: true},
       })
     return new NextResponse(JSON.stringify(comments, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
+
+// CREATE COMMENT OF A POST
+export const POST = async (req) => {
+  const session = await getAuthSession();
+
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+    );
+  }
+
+  try {
+    const body = await req.json();
+    const comment = await prisma.comment.create({
+      data: { ...body, userEmail: session.user.email },
+    });
+
+    return new NextResponse(JSON.stringify(comment, { status: 200 }));
   } catch (err) {
     console.log(err);
     return new NextResponse(
