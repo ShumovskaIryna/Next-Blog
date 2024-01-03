8892daee617b7f5e6e0c9da064dd3efe82eb11fc
Iryna Shumovska <shumovska_ia@groupbwt.com>
2024-01-03 22:41:41 +0200

    add firebase for upload file

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -20,7 +20,7 @@ const SinglePage = async ({ params }) => {
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>{data.title}</h1>
+          <h1 className={styles.title}>{data?.title}</h1>
           <div className={styles.user}>
             {data?.user && (
               <div className={styles.userImageContainer}>
@@ -35,7 +35,7 @@ const SinglePage = async ({ params }) => {
             <div className={styles.userTextContainer}>
               <span className={styles.username}>{data?.user?.name}</span>
               <span className={styles.date}>
-                {data.createdAt.substring(0, 10)}
+                {data?.createdAt.substring(0, 10)}
               </span>
             </div>
           </div>
@@ -43,7 +43,7 @@ const SinglePage = async ({ params }) => {
         {data?.img && (
           <div className={styles.imageContainer}>
             <Image
-              src={data.img}
+              src={data?.img}
               alt="postImage"
               fill
               className={styles.image}
--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -6,6 +6,13 @@ import { useEffect, useState } from "react";
 import "react-quill/dist/quill.bubble.css";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "@/utils/firebase";
 import ReactQuill from "react-quill";
 
 const WritePage = () => {
@@ -14,6 +21,43 @@ const WritePage = () => {
 
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
+  const [file, setFile] = useState(null);
+  const [media, setMedia] = useState("");
+  const [title, setTitle] = useState("");
+  const [categorySlug, setCategorySlug] = useState("");
+
+  useEffect(() => {
+    const storage = getStorage(app);
+    const upload = () => {
+      const name = new Date().getTime() + file.name;
+      const storageRef = ref(storage, name);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {},
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setMedia(downloadURL);
+          });
+        }
+      );
+    };
+    file && upload();
+  }, [file]);
 
   if (status === "loading") {
     return <div className={styles.loading}>Loading...</div>;
@@ -23,16 +67,46 @@ const WritePage = () => {
     router.push("/");
   }
 
+  const slugify = (str) =>
+    str
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, "")
+      .replace(/[\s_-]+/g, "-")
+      .replace(/^-+|-+$/g, "");
+
+  const handleSubmit = async () => {
+    const res = await fetch("/api/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        desc: value,
+        img: media,
+        slug: slugify(title),
+        categorySlug: categorySlug || "entertaiment", //If not selected, choose the general category
+      }),
+    });
+    console.log(res);
+  };
+
   return (
     <div className={styles.container}>
-      <input type="text" placeholder="Title" className={styles.input} />
-      <select className={styles.select}>
-        <option value="style">style</option>
-        <option value="fashion">fashion</option>
+      <input
+        type="text"
+        placeholder="Title"
+        className={styles.input}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <select
+        className={styles.select}
+        onChange={(e) => setCategorySlug(e.target.value)}
+      >
+        <option value="entertaiment">entertaiment</option>
+        <option value="study">study</option>
         <option value="food">food</option>
-        <option value="culture">culture</option>
-        <option value="travel">travel</option>
         <option value="coding">coding</option>
+        <option value="travel">travel</option>
+        <option value="sport">sport</option>
       </select>
       <div className={styles.editor}>
         <button className={styles.button} onClick={() => setOpen(!open)}>
@@ -40,7 +114,12 @@ const WritePage = () => {
         </button>
         {open && (
           <div className={styles.add}>
-            <input type="file" id="image" style={{ display: "none" }} />
+            <input
+              type="file"
+              id="image"
+              onChange={(e) => setFile(e.target.files[0])}
+              style={{ display: "none" }}
+            />
             <button className={styles.addButton}>
               <label htmlFor="image">
                 <Image src="/image.png" alt="" width={16} height={16} />
@@ -58,10 +137,13 @@ const WritePage = () => {
           className={styles.textArea}
           theme="bubble"
           value={value}
+          onChange={setValue}
           placeholder="Tell your story..."
         />
       </div>
-      <button className={styles.publish}>Publish</button>
+      <button className={styles.publish} onClick={handleSubmit}>
+        Publish
+      </button>
     </div>
   );
 };
